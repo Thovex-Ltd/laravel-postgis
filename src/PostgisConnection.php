75e7385acd66775bf80ac55b3a9fe5f3c862e29b
Alistair Reynolds <alistair@thovex.co.uk>
2026-04-13 11:31:05 +0100

    Fix Grammar constructor for Laravel 12 compatibility
    Laravel 12 changed Grammar::__construct() to require a Connection
    argument. Pass $this (the connection) when instantiating PostgisGrammar.

--- src/PostgisConnection.php
+++ src/PostgisConnection.php
@@ -14,7 +14,7 @@ class PostgisConnection extends BasePostgresConnection
      */
     protected function getDefaultSchemaGrammar()
     {
-        return $this->withTablePrefix(new PostgisGrammar());
+        return $this->withTablePrefix(new PostgisGrammar($this));
     }
 
     /**
